0cacf0668d26209f6375f764665a9b3c103e220c
Owen Auch <owenauch@wustl.edu>
2017-02-23 02:31:11 -0600

    Works with no end case

--- doubtingthomas.py
+++ doubtingthomas.py
@@ -8,12 +8,13 @@ from urllib2 import urlopen
 import re
 import ssl
 from sys import version_info
+import re
 
 #base url
 base = "https://www.biblegateway.com/passage/"
 
 #verse to start our journey with
-start_verse = "John 10:10"
+start_verse_name = "John 10:10"
 
 # list of all verse found so far
 verse_list = []
@@ -34,13 +35,11 @@ class Verse:
 	# get book name from url query string
 	def get_book(self):
 		book = self.url.split("+", 1)[0]
-		print book
 		return book
 
 	# get chapter name from url query string
 	def get_chapter(self):
 		chapter = self.url.split("+", 1)[1].split("%", 1)[0]
-		print chapter
 		return chapter
 
 	# gets verse from the url query string
@@ -55,14 +54,12 @@ class Verse:
 
 	# adds several references out to refsOut array
 	# need to pass in an array of Verse objects
-	def add_ref_out(self, refsOut):
-		for verse in refsOut:
-			self.refsOut.append(verse)
+	def add_ref_out(self, refOut):
+		self.refsOut.append(refOut)
 
 	#get full url for searching
 	def get_full_url(self):
 		return self.baseurl + self.url
-
 
 
 #verify set to false for scrape
@@ -75,7 +72,7 @@ def verify_false():
 # create verse object for first verse and place in verse_list
 def make_first_verse(verse):
 	url = verse.replace(" ", "+")
-	url = verse.replace(":", "%3A")
+	url = url.replace(":", "%3A")
 	start_verse = Verse(url)
 	verse_list.append(start_verse)
 	return start_verse
@@ -83,17 +80,46 @@ def make_first_verse(verse):
 #open and soupify verse
 def soupify(verse):
     ctx = verify_false()
-    html = urlopen(verse.url, context=ctx).read()
+    url = verse.get_full_url()
+    html = urlopen(url, context=ctx).read()
     soup = BeautifulSoup(html, "lxml")
     return soup
 
-#get link for first cross-referenced verse
-def get_next_url(soup):
-    crossref_div = soup.find('div', class_='crossrefs')
-    return crossref_div
+#parses a long url of multiple verses into seperate verse urls
+def parse_crossref_url(href):
+    urls = href.split('search=', 1)[1].split('%2C')
+    urls[-1] = urls[-1].split('&version', 1)[0]
+    return urls
+
+#the biggie
+#gets all the verses crossreferenced by the input verse,
+#saves then to verse_list, then calls get_next_verses on all of them
+def get_next_verses(verse):
+    soup = soupify(verse)
+    crossref_div = soup.find("div", class_="crossrefs")
+
+    #gets all list elements in crossref_div
+    crossref_li = crossref_div.find_all("li")
+
+    #gets href for each a element to a cross-referenced verse
+    #puts in list hrefs_list
+    #each href is in the form /passage/?search=<verse1url>%2C<verse2url>&version=NIV
+    hrefs_list = []
+    for li in crossref_li:
+        full_link = li.find("a", class_="crossref-link")['href']
+        hrefs_list.append(full_link)
+
+    #get all the reference urls, make them into verses
+    #save properties of them, save them to verse list, and call on them
+    for ref in hrefs_list:
+        refs_list = parse_crossref_url(ref)
+        for ref in refs_list:
+            new_verse = Verse(ref)
+            new_verse.add_ref_in(verse)
+            verse.add_ref_out(new_verse)
+            get_next_verses(new_verse)
+
 
 #first step
-start = make_first_verse(start_verse)
-print start.url
-soup = soupify(start)
-print (soup)
+start_verse = make_first_verse(start_verse_name)
+get_next_verses(start_verse)
